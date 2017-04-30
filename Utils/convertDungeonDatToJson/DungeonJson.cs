aca4dfd9509ca73399e5ed4ac579d05390449a29
kenjiuno <h8mastre@gmail.com>
2017-04-30 22:34:03 +0900

    Adding convertDungeonDatToJson under development.

---
+++ Utils/convertDungeonDatToJson/DungeonJson.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DM2Internal {
+    public class DungeonJson {
+        public int randomGraphicsSeed;
+        public DirPos startingPartyPosition;
+        public List<MapDef> maps = new List<MapDef>();
+        public List<ObjDef> objs = new List<ObjDef>();
+    }
+
+    public class MapDef {
+        [NonSerialized]
+        public int mapOffset;
+        [NonSerialized]
+        public int currentNumberOfColumns;
+        [NonSerialized]
+        public int numWallGraphs;
+        [NonSerialized]
+        public int numWallRandom;
+        [NonSerialized]
+        public int numFloorGraphs;
+        [NonSerialized]
+        public int numFloorRandom;
+        [NonSerialized]
+        public int numDoorDecorationGraphics;
+        [NonSerialized]
+        public int numCreaturesTypes;
+        [NonSerialized]
+        public int doorType0;
+        [NonSerialized]
+        public int doorType1;
+        [NonSerialized]
+        public List<int> creaturesTypes = new List<int>();
+        [NonSerialized]
+        public List<int> wallGraphs = new List<int>();
+        [NonSerialized]
+        public List<int> floorGraphs = new List<int>();
+        [NonSerialized]
+        public List<int> doorDecorationGraphics = new List<int>();
+
+        /// <summary>
+        /// in dm style. hexstr: 00 to FF. top to down, and then left to right.
+        /// </summary>
+        public string tiles;
+
+        /// <summary>
+        /// height*width, top down, then left-to-right.
+        /// -1 if no obj.
+        /// </summary>
+        public int[] objRefs;
+
+        public bool useTeleporter;
+        public bool useDoor0;
+        public bool useDoor1;
+        public bool useUpperPitfall;
+        public bool useLowerPitfall;
+        public bool useUpStaircase;
+        public bool useDownStaircase;
+        public byte shiftX;
+        public byte shiftY;
+        public int height;
+        public int width;
+        public int level;
+        public int difficulty;
+        public int mapGraphicsStyle;
+    }
+
+    public class ObjDef {
+        public int direction;
+        public int nextObjRef = -1;
+    }
+    public class DoorDef : ObjDef {
+        public String type = "door";
+        public bool buttonState;
+        public bool bashablebyChopping;
+        public bool destroyablebyFireball;
+        public bool button;
+        public bool openVertical;
+        /// <summary>
+        /// real door type index in gdat.
+        /// </summary>
+        public int doorType;
+        /// <summary>
+        /// real door ornate index in gdat. -1 has no one.
+        /// </summary>
+        public int doorOrnate = -1;
+
+        public bool openingDir;
+    }
+    public class TeleporterDef : ObjDef {
+        public String type = "teleporter";
+        public MapPos destination;
+        public int rotation;
+        public bool absoluteRotation;
+        public int scope;
+        public bool sound;
+    }
+    public class TextDef : ObjDef {
+        public String type = "text";
+        public bool textVisibility;
+        public int textMode;
+        /// <summary>
+        /// either: textIndex, wall ornate or floor ornate.
+        /// </summary>
+        public int textIndex;
+    }
+    public class ActuatorDef : ObjDef {
+        public String type = "actuator";
+
+        public int actuatorType;
+        public int actuatorData;
+        public bool activeStatus;
+        public bool onceOnlyActuator;
+        public int actionType;
+        public bool inversion;
+        public bool sound;
+        /// <summary>
+        /// delay or ShopItemPoolNo
+        /// </summary>
+        public int delay;
+        public int graphicNumber;
+        public int newDirection;
+        public int xCoord;
+        public int yCoord;
+    }
+    public class CreatureDef : ObjDef {
+        public String type = "creature";
+        public int childObjRef = -1;
+    }
+    public class WeaponDef : ObjDef {
+        public String type = "weapon";
+    }
+    public class ClothDef : ObjDef {
+        public String type = "cloth";
+    }
+    public class ScrollDef : ObjDef {
+        public String type = "scroll";
+    }
+    public class PotionDef : ObjDef {
+        public String type = "potion";
+    }
+    public class ContainerDef : ObjDef {
+        public String type = "container";
+        public int childObjRef = -1;
+    }
+    public class MiscItemDef : ObjDef {
+        public String type = "miscItem";
+    }
+    public class MissileDef : ObjDef {
+        public String type = "missile";
+        public int childObjRef = -1;
+    }
+    public class CloudDef : ObjDef {
+        public String type = "cloud";
+    }
+
+    public class DirPos {
+        /// <summary>
+        /// 0,1,2,3 = N,E,S,W
+        /// </summary>
+        public int dir;
+
+        /// <summary>
+        /// 0 based, horizontal, left to right
+        /// </summary>
+        public int x;
+
+        /// <summary>
+        /// 0 based, vertical, top to bottom
+        /// </summary>
+        public int y;
+
+        public DirPos(ushort position) {
+            dir = (position >> 10) & 3;
+            x = (position) & 31;
+            y = (position >> 5) & 31;
+        }
+    }
+
+    public class MapPos {
+        /// <summary>
+        /// zero based
+        /// </summary>
+        public int map;
+
+        /// <summary>
+        /// 0 based, horizontal, left to right
+        /// </summary>
+        public int x;
+
+        /// <summary>
+        /// 0 based, vertical, top to bottom
+        /// </summary>
+        public int y;
+
+        public MapPos() { }
+
+        public MapPos(ushort position) {
+            map = (position >> 10) & 63;
+            x = (position) & 31;
+            y = (position >> 5) & 31;
+        }
+    }
+
+    public enum dbIndex {
+        dbDoor = 0,				// 0
+        dbTeleporter,			// 1
+        dbText,					// 2
+        dbActuator,				// 3
+        dbCreature,				// 4
+        dbWeapon,				// 5
+        dbCloth,				// 6
+        dbScroll,				// 7
+        dbPotion,				// 8
+        dbContainer,			// 9
+        dbMiscellaneous_item,	//10
+        db11,
+        db12,
+        db13,
+        dbMissile,				//14
+        dbCloud,				//15
+        dbMax = 16,
+    }
+
+    public enum teleporterScope { // teleporter scope
+        items = 0,
+        creatures = 1,
+        itemsParty = 2,
+        everything = 3,
+    }
+}
---
+++ Utils/convertDungeonDatToJson/Program.cs
@@ -0,0 +1,475 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Diagnostics;
+using DM2Internal;
+using Newtonsoft.Json;
+
+namespace convertDungeonDatToJson {
+    class Program {
+        static void Main(string[] args) {
+            if (args.Length < 2) {
+                Console.Error.WriteLine("convertDungeonDatToJson in.dat out.json ");
+                Console.Error.WriteLine("convertDungeonDatToJson dungeon.dat dungeon.json ");
+                Environment.ExitCode = 1;
+                return;
+            }
+            new Program().Run(args[0], args[1]);
+        }
+
+        FileStream si;
+        MemoryStream os;
+        BinaryReader br;
+        BinaryWriter wr;
+
+        public System.UInt16 RandomGraphicsSeed;
+        public System.UInt16 MapDataSize;
+        public System.Byte NumberOfLevels;
+        public System.UInt16 TextDataSize;
+        public System.UInt16 StartingPartyPosition;
+        public System.UInt16 ObjectListSize;
+        public System.UInt16 NumberOfDoors;
+        public System.UInt16 NumberOfTeleporters;
+        public System.UInt16 NumberOfTexts;
+        public System.UInt16 NumberOfActuators;
+        public System.UInt16 NumberOfCreatures;
+        public System.UInt16 NumberOfWeapons;
+        public System.UInt16 NumberOfClothes;
+        public System.UInt16 NumberOfScrolls;
+        public System.UInt16 NumberOfPotions;
+        public System.UInt16 NumberOfChests;
+        public System.UInt16 NumberOfMiscItems;
+        public System.UInt16 NumberOf11;
+        public System.UInt16 NumberOf12;
+        public System.UInt16 NumberOf13;
+        public System.UInt16 NumberOfMissile;
+        public System.UInt16 NumberOfCloud;
+        // 
+        public int NumberOfColumns;
+
+        DungeonJson d = new DungeonJson();
+        List<long> offsetsToDatabase = new List<long>();
+
+        private void Run(String fpin, String fpout) {
+            using (si = File.OpenRead(fpin)) {
+                br = new BinaryReader(si);
+                os = new MemoryStream();
+                wr = new BinaryWriter(os);
+
+                // http://dmweb.free.fr/?q=node/217
+
+                d.randomGraphicsSeed = RandomGraphicsSeed = DoWord();
+                MapDataSize = DoWord();
+                NumberOfLevels = DoByte();
+                DoByte();
+                TextDataSize = DoWord();
+                d.startingPartyPosition = new DirPos(StartingPartyPosition = DoWord());
+                ObjectListSize = DoWord();
+                NumberOfDoors = DoWord();
+                NumberOfTeleporters = DoWord();
+                NumberOfTexts = DoWord();
+                NumberOfActuators = DoWord();
+                NumberOfCreatures = DoWord();
+                NumberOfWeapons = DoWord();
+                NumberOfClothes = DoWord();
+                NumberOfScrolls = DoWord();
+                NumberOfPotions = DoWord();
+                NumberOfChests = DoWord();
+                NumberOfMiscItems = DoWord();
+                NumberOf11 = DoWord();
+                NumberOf12 = DoWord();
+                NumberOf13 = DoWord();
+                NumberOfMissile = DoWord();
+                NumberOfCloud = DoWord();
+
+                int i;
+                NumberOfColumns = 0;
+                for (i = 0; i < NumberOfLevels; i++) {
+                    MapDef mapdef = new MapDef();
+                    mapdef.mapOffset = DoWord();
+                    int w2 = DoWord();
+                    mapdef.useTeleporter = 0 != ((w2 >> 5) & 1);
+                    mapdef.useDoor0 = 0 != ((w2 >> 7) & 1);
+                    mapdef.useDoor1 = 0 != ((w2 >> 8) & 1);
+                    mapdef.useUpperPitfall = (w2 & 1) != 0;
+                    mapdef.useLowerPitfall = ((w2 >> 1) & 1) != 0;
+                    mapdef.useUpStaircase = ((w2 >> 3) & 1) != 0;
+                    mapdef.useDownStaircase = ((w2 >> 4) & 1) != 0;
+
+                    DoByte();
+                    DoByte();
+                    mapdef.shiftX = DoByte();
+                    mapdef.shiftY = DoByte();
+                    int mapSize = DoWord();
+                    mapdef.height = ((mapSize >> 11) & 31) + 1;
+                    mapdef.width = ((mapSize >> 6) & 31) + 1;
+                    mapdef.level = ((mapSize) & 63);
+                    mapdef.currentNumberOfColumns = NumberOfColumns;
+                    NumberOfColumns += mapdef.width;
+
+                    int numGraphs = DoWord();
+                    mapdef.numWallGraphs = (numGraphs) & 15;
+                    mapdef.numWallRandom = (numGraphs >> 4) & 15;
+                    mapdef.numFloorGraphs = (numGraphs >> 8) & 15;
+                    mapdef.numFloorRandom = (numGraphs >> 12) & 15;
+
+                    int w12 = DoWord();
+                    mapdef.numDoorDecorationGraphics = (w12) & 15;
+                    mapdef.numCreaturesTypes = (w12 >> 4) & 15;
+                    mapdef.difficulty = (w12 >> 12) & 15;
+
+                    int w14 = DoWord();
+                    mapdef.mapGraphicsStyle = (w14 >> 4) & 15;
+                    mapdef.doorType0 = (w14 >> 8) & 15;
+                    mapdef.doorType1 = (w14 >> 12) & 15;
+
+                    mapdef.objRefs = new int[mapdef.height * mapdef.width];
+                    for (int t = 0; t < mapdef.objRefs.Length; t++) {
+                        mapdef.objRefs[t] = -1;
+                    }
+
+                    d.maps.Add(mapdef);
+                }
+
+                List<int> columns = new List<int>();
+                List<int> objectList = new List<int>();
+
+                for (i = 0; i < NumberOfColumns; i++) {
+                    columns.Add(DoWord());
+                }
+                for (i = 0; i < ObjectListSize; i++) {
+                    objectList.Add(DoWord());
+                }
+                for (i = 0; i < TextDataSize; i++) {
+                    DoWord();
+                }
+                offsetsToDatabase.Add(si.Position);
+                for (i = 0; i < NumberOfDoors; i++) {
+                    DoWord();
+                    DoWord();
+                }
+                offsetsToDatabase.Add(si.Position);
+                for (i = 0; i < NumberOfTeleporters; i++) {
+                    DoWord();
+                    DoWord();
+                    DoWord();
+                }
+                offsetsToDatabase.Add(si.Position);
+                for (i = 0; i < NumberOfTexts; i++) {
+                    DoWord();
+                    DoWord();
+                }
+                offsetsToDatabase.Add(si.Position);
+                for (i = 0; i < NumberOfActuators; i++) {
+                    DoWord();
+                    DoWord();
+                    DoWord();
+                    DoWord();
+                }
+                offsetsToDatabase.Add(si.Position);
+                for (i = 0; i < NumberOfCreatures; i++) {
+                    DoWord();
+                    DoWord();
+                    DoByte();
+                    DoByte();
+                    DoWord();
+                    DoWord();
+                    DoWord();
+                    DoWord();
+                    DoWord();
+                    //
+                    MyDWord();
+                }
+                offsetsToDatabase.Add(si.Position);
+                for (i = 0; i < NumberOfWeapons; i++) {
+                    DoWord();
+                    DoWord();
+                }
+                offsetsToDatabase.Add(si.Position);
+                for (i = 0; i < NumberOfClothes; i++) {
+                    DoWord();
+                    DoWord();
+                }
+                offsetsToDatabase.Add(si.Position);
+                for (i = 0; i < NumberOfScrolls; i++) {
+                    DoWord();
+                    DoWord();
+                }
+                offsetsToDatabase.Add(si.Position);
+                for (i = 0; i < NumberOfPotions; i++) {
+                    DoWord();
+                    DoWord();
+                }
+                offsetsToDatabase.Add(si.Position);
+                for (i = 0; i < NumberOfChests; i++) {
+                    DoWord();
+                    DoWord();
+                    DoByte();
+                    DoByte();
+                    DoWord();
+                }
+                offsetsToDatabase.Add(si.Position);
+                for (i = 0; i < NumberOfMiscItems; i++) {
+                    DoWord();
+                    DoWord();
+                }
+                offsetsToDatabase.Add(si.Position);
+                for (i = 0; i < NumberOf11; i++) {
+                    DoWord();
+                    DoWord();
+                    DoWord();
+                    DoWord();
+                }
+                offsetsToDatabase.Add(si.Position);
+                for (i = 0; i < NumberOf12; i++) {
+                    DoWord();
+                    DoWord();
+                    DoWord();
+                    DoWord();
+                }
+                offsetsToDatabase.Add(si.Position);
+                for (i = 0; i < NumberOf13; i++) {
+                    DoWord();
+                    DoWord();
+                    DoWord();
+                    DoWord();
+                }
+                offsetsToDatabase.Add(si.Position);
+                for (i = 0; i < NumberOfMissile; i++) {
+                    DoWord();
+                    DoWord();
+                    DoByte();
+                    DoByte();
+                    DoWord();
+                }
+                offsetsToDatabase.Add(si.Position);
+                for (i = 0; i < NumberOfCloud; i++) {
+                    DoWord();
+                    DoWord();
+                }
+
+                Int64 offMapIn = si.Position;
+
+                for (int z = 0; z < d.maps.Count; z++) {
+                    var mapdef = d.maps[z];
+                    si.Position = offMapIn + mapdef.mapOffset + mapdef.width * mapdef.height;
+                    for (int t = 0; t < mapdef.numCreaturesTypes; t++) {
+                        mapdef.creaturesTypes.Add(br.ReadByte());
+                    }
+                    for (int t = 0; t < mapdef.numWallGraphs; t++) {
+                        mapdef.wallGraphs.Add(br.ReadByte());
+                    }
+                    for (int t = 0; t < mapdef.numFloorGraphs; t++) {
+                        mapdef.floorGraphs.Add(br.ReadByte());
+                    }
+                    for (int t = 0; t < mapdef.numDoorDecorationGraphics; t++) {
+                        mapdef.doorDecorationGraphics.Add(br.ReadByte());
+                    }
+                    mapdef.doorDecorationGraphics.Add(0);
+
+                    si.Position = offMapIn + mapdef.mapOffset;
+                    String tiles = "";
+                    int tilePos = 0;
+                    for (int x = 0; x < mapdef.width; x++) {
+                        int currentObjectListIndex = columns[mapdef.currentNumberOfColumns + x];
+                        for (int y = 0; y < mapdef.height; y++, tilePos++) {
+                            byte tile = br.ReadByte();
+                            tiles += tile.ToString("X2");
+                            bool hasObj = 0 != (tile & 8);
+                            if (hasObj) {
+                                int oid = objectList[currentObjectListIndex];
+                                if (oid != oidFree) {
+                                    mapdef.objRefs[tilePos] = convertObject(mapdef, oid);
+                                }
+
+                                currentObjectListIndex++;
+                            }
+                        }
+                    }
+                    mapdef.tiles = tiles;
+                }
+
+                using (StreamWriter writer = new StreamWriter(fpout)) {
+                    using (JsonTextWriter jsonWriter = new JsonTextWriter(writer)) {
+                        jsonWriter.Formatting = Formatting.Indented;
+                        new JsonSerializer().Serialize(jsonWriter, d);
+                    }
+                }
+            }
+        }
+
+        int[] dbSizes = new int[] { 4, 6, 4, 8, 16, 4, 4, 4, 4, 8, 4, 0, 0, 0, 8, 4 };
+
+        private int convertObject(MapDef mapdef, int oid) {
+            Trace.Assert(oid != oidFree);
+            Trace.Assert(oid != oidEnd);
+            int dir = (oid >> 14) & 3;
+            int cat = (oid >> 10) & 15;
+            int num = (oid) & 1023;
+            int objRef = d.objs.Count;
+            ObjDef newObj = null;
+            si.Position = offsetsToDatabase[cat] + dbSizes[cat] * num;
+            int nextOid = br.ReadUInt16();
+            switch ((dbIndex)cat) {
+                case dbIndex.dbDoor: {
+                        int attr = br.ReadUInt16();
+                        bool doorType1 = 0 != (attr & 1);
+                        int ornateIndex = (attr >> 1) & 15;
+
+                        d.objs.Add(newObj = new DoorDef {
+                            doorType = doorType1 ? mapdef.doorType1 : mapdef.doorType0,
+                            doorOrnate = mapdef.doorDecorationGraphics[ornateIndex],
+                            openVertical = 0 != ((attr >> 5) & 1),
+                            button = 0 != ((attr >> 6) & 1),
+                            destroyablebyFireball = 0 != ((attr >> 7) & 1),
+                            bashablebyChopping = 0 != ((attr >> 8) & 1),
+                            //9
+                            //10
+                            //11
+                            buttonState = 0 != ((attr >> 12) & 1),
+                            //13
+                            //14
+                            //15
+                        });
+                        break;
+                    }
+                case dbIndex.dbTeleporter: {
+                        int attr = br.ReadUInt16();
+                        int attr2 = br.ReadUInt16();
+                        d.objs.Add(newObj = new TeleporterDef {
+                            destination = new MapPos {
+                                map = (attr2 >> 8) & 255,
+                                x = (attr >> 0) & 31,
+                                y = (attr >> 5) & 31,
+                            },
+                            rotation = (attr >> 10) & 3,
+                            absoluteRotation = 0 != ((attr >> 12) & 1),
+                            scope = (attr >> 13) & 3,
+                            sound = 0 != ((attr >> 15) & 1),
+                        });
+                        break;
+                    }
+                case dbIndex.dbText: {
+                        int attr = br.ReadUInt16();
+                        d.objs.Add(newObj = new TextDef {
+                            textVisibility = 0 != ((attr >> 0) & 1),
+                            textMode = (attr >> 1) & 3,
+                            textIndex = (attr >> 3) & 0x1fff,
+                        });
+                        break;
+                    }
+                case dbIndex.dbActuator: {
+                        int attr = br.ReadUInt16();
+                        int attr2 = br.ReadUInt16();
+                        int attr3 = br.ReadUInt16();
+                        d.objs.Add(newObj = new ActuatorDef {
+                            actuatorType = (attr >> 0) & 127,
+                            actuatorData = (attr >> 7) & 511,
+
+                            onceOnlyActuator = 0 != ((attr2 >> 2) & 1),
+                            actionType = (attr2 >> 3) & 3,
+                            inversion = 0 != ((attr2 >> 5) & 1),
+                            sound = 0 != ((attr2 >> 6) & 1),
+                            delay = (attr2 >> 7) & 15,
+                            graphicNumber = (attr2 >> 12) & 15,
+
+                            newDirection = (attr3 >> 4) & 3,
+                            xCoord = (attr3 >> 6) & 31,
+                            yCoord = (attr3 >> 11) & 31,
+                        });
+                        break;
+                    }
+                case dbIndex.dbCreature: {
+                        int childOid = br.ReadUInt16();
+                        CreatureDef me;
+                        d.objs.Add(newObj = me = new CreatureDef {
+                            childObjRef = (childOid != oidEnd) ? convertObject(mapdef, childOid) : -1,
+                        });
+                        break;
+                    }
+                case dbIndex.dbWeapon: {
+                        d.objs.Add(newObj = new WeaponDef {
+                        });
+                        break;
+                    }
+                case dbIndex.dbCloth: {
+                        d.objs.Add(newObj = new ClothDef {
+                        });
+                        break;
+                    }
+                case dbIndex.dbScroll: {
+                        d.objs.Add(newObj = new ScrollDef {
+                        });
+                        break;
+                    }
+                case dbIndex.dbPotion: {
+                        d.objs.Add(newObj = new PotionDef {
+                        });
+                        break;
+                    }
+                case dbIndex.dbContainer: {
+                        int childOid = br.ReadUInt16();
+                        ContainerDef me;
+                        d.objs.Add(newObj = me = new ContainerDef {
+                            childObjRef = (childOid != oidEnd) ? convertObject(mapdef, childOid) : -1,
+                        });
+                        break;
+                    }
+                case dbIndex.dbMiscellaneous_item: {
+                        d.objs.Add(newObj = new MiscItemDef {
+                        });
+                        break;
+                    }
+                case dbIndex.dbMissile: {
+                        int childOid = br.ReadUInt16();
+                        MissileDef me;
+                        d.objs.Add(newObj = me = new MissileDef {
+                            childObjRef = (childOid != oidEnd) ? convertObject(mapdef, childOid) : -1,
+                        });
+                        break;
+                    }
+                case dbIndex.dbCloud: {
+                        d.objs.Add(newObj = new CloudDef {
+                        });
+                        break;
+                    }
+                default:
+                    throw new NotSupportedException();
+            }
+            newObj.direction = dir;
+            if (nextOid != oidEnd) {
+                newObj.nextObjRef = convertObject(mapdef, nextOid);
+            }
+            return objRef;
+        }
+
+        int oidFree = 0xFFFF;
+        int oidEnd = 0xFFFE;
+
+        private void MyDWord() {
+            wr.Write((uint)0);
+        }
+        private void MyWord() {
+            wr.Write((ushort)0);
+        }
+
+        private void DoBytes(int cb) {
+            byte[] bin = new byte[cb];
+            if (si.Read(bin, 0, cb) != cb) throw new EndOfStreamException();
+            os.Write(bin, 0, cb);
+        }
+
+        private byte DoByte() {
+            byte r;
+            wr.Write(r = br.ReadByte());
+            return r;
+        }
+
+        private ushort DoWord() {
+            ushort r;
+            wr.Write(r = br.ReadUInt16());
+            return r;
+        }
+    }
+}
